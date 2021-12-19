58e38df05d15450f33642e54e652edde7ae00979
xi.nie <xi.nie@outlook.com>
2021-12-19 17:06:43 +0800

    add day19

--- rust/src/advcode2021.rs
+++ rust/src/advcode2021.rs
@@ -1865,9 +1865,6 @@ pub fn p18() {
             }
         }
         println!("{} => {} {} = {}", max, st.magnitude(), st, res_s);
-
-
-
     }
 }
 
@@ -2058,3 +2055,252 @@ impl std::fmt::Display for P18Pair {
         write!(f, "[{},{}]", self.left, self.right)
     }
 }
+
+pub fn p19() {
+    let contents = r#"--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
+
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
+
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14"#;
+    let _contents = std::fs::read_to_string("./assets/adv2021/adv19.txt").unwrap(); let contents: &str = &_contents;
+
+    let (_, scanners) = p19_parse(contents.trim()).unwrap();
+
+    assert!(scanners.len() > 0);
+    let mut map = scanners[0].clone();
+    let mut walked: HashSet<_> = (1..scanners.len()).into_iter().collect();
+    let mut positions = vec![(0, 0, 0)];
+
+    while walked.len() > 0 {
+        let mut found = false;
+        for &idx in walked.iter() {
+            if let Some(shift) = p19_try(&mut map, &scanners[idx]) {
+                found = true;
+                walked.remove(&idx);
+                println!("Add: {}, Left: {}", idx, walked.len());
+                positions.push(shift);
+                break;
+            }
+        }
+
+        assert!(found, "no solution found");
+    }
+
+    let mut distance = 0;
+    for i in 0..positions.len() {
+        for j in 0..positions.len() {
+            let (sx, sy, sz) = positions[i];
+            let (ex, ey, ez) = positions[j];
+            distance = distance.max((sx-ex).abs() + (sy-ey).abs() + (sz-ez).abs());
+        }
+    }
+
+    println!("{}: {:?}", map.len(), map);
+    println!("Scanners {}: {:?}", distance, positions);
+}
+
+fn p19_rotate((sx, sy, sz): (i64, i64, i64), dir: &[[i64; 3]; 3]) -> (i64, i64, i64) {
+    (
+        sx*dir[0][0]+sy*dir[0][1]+sz*dir[0][2],
+        sx*dir[1][0]+sy*dir[1][1]+sz*dir[1][2],
+        sx*dir[2][0]+sy*dir[2][1]+sz*dir[2][2],
+    )
+}
+
+fn p19_move_to((fx, fy, fz): (i64, i64, i64), (sx, sy, sz): (i64, i64, i64)) -> (i64, i64, i64) {
+    (fx+sx, fy+sy, fz+sz)
+}
+
+fn p19_try(map: &mut Vec<(i64, i64, i64)>, to: &Vec<(i64, i64, i64)>) -> Option<(i64, i64, i64)> {
+    let map_s: HashSet<_> = map.iter().map(Clone::clone).collect();
+    for idx in 0..3 {
+        for sigx in [-1, 1] {
+            for idy in 0..3 {
+                for sigy in [-1, 1] {
+                    for idz in 0..3 {
+                        for sigz in [-1, 1] {
+                            if idx == idy || idy == idz || idx == idz { continue; }
+                            let mut rotate = [[0; 3]; 3];
+                            rotate[0][idx] = sigx;
+                            rotate[1][idy] = sigy;
+                            rotate[2][idz] = sigz;
+
+                            let curr: Vec<_> = to.iter().map(|p| p19_rotate(p.clone(), &rotate)).collect();
+
+                            for idi in 0..curr.len() {
+                                for idj in 0..map.len() {
+                                    let (tx, ty, tz) = curr[idi];
+                                    let (sx, sy, sz) = map[idj];
+                                    let shift = (sx-tx, sy-ty, sz-tz);
+
+                                    let curr: HashSet<_> = curr.iter().map(|p| p19_move_to(p.clone(), shift)).collect();
+
+                                    if map_s.intersection(&curr).into_iter().count() >= 12 {
+                                        for item in curr.difference(&map_s) {
+                                            map.push(item.clone());
+                                        }
+                                        return Some(shift);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    None
+}
+
+fn p19_parse(input: &str) -> IResult<&str, Vec<Vec<(i64, i64, i64)>>> {
+    use nom::multi::{separated_list0, separated_list1, many1};
+    use nom::character::complete::{newline, i64 as i64_p, digit1};
+    use nom::bytes::complete::tag;
+    use nom::sequence::tuple;
+    use nom::combinator::map;
+
+    // let scaner = |input: &str| -> IResult<&str, Vec<(i64, i64, i64)>> {
+    let scaner = |input| {
+        let (input, _) = tag("--- scanner ")(input)?;
+        let (input, _) = digit1(input)?;
+        let (input, _) = tag(" ---")(input)?;
+        let (input, _) = newline(input)?;
+        let tup = map(tuple((i64_p, tag(","), i64_p, tag(","), i64_p)), |(i1, _, i2, _, i3)| (i1, i2, i3));
+
+        separated_list1(newline, tup)(input)
+    };
+
+    separated_list0(many1(newline), scaner)(input)}
